632d320e7a96db6f658dc033b80b4986b2f0b35d
ngodi <albertngdi@gmail.com>
2020-09-16 23:14:50 +0100

    covid dignosis section

--- e-care/src/Components/CovidLive/CovidLive.js
+++ e-care/src/Components/CovidLive/CovidLive.js
@@ -32,14 +32,34 @@ export default () => {
                 <img src='./Images/googlemap.png' alt="google maps placeholder" width="100%" />
             </div>
             <div className="covid-alert">
-                Alert! 20 People Suspectd Positive
+                Alert! 20 People Suspected Positive
             </div>
             <div className="filter-covid">
-                <button>500m</button>
-                <button>1 Km </button>
-                <button>5 Km </button>
-                <button> 10 Km </button>
->            </div>
+                <button className="filter-button">500m</button>
+                <button className="filter-button-inverse">1 Km </button>
+                <button className="filter-button">5 Km </button>
+                <button className="filter-button"> 10 Km </button>
+            </div>
+            <div className="self-diagnosis">
+                <span className="sub-heading">Diagnose Yourself</span>
+                <div className="check-symptoms">
+                    <ul className="covid-symptoms-list">
+                        <li><span className="covid-symptom">Suffering from High Fever</span></li>
+                        <li><span className="covid-symptom">Suffering from Cold and Cough</span>
+                            <span className="covid-symptom">Have Breathing Problem</span>  
+                        </li>
+                        <li><span className="covid-symptom">Travelled from Abroad</span></li>
+                        <li><span className="covid-symptom">Visit someone who has recently tested positive</span></li>
+                        <li><span className="covid-symptom-selected">None of the Above</span></li>
+                    </ul>
+                    <div className="diagnosis-feedback">
+                        <p>Congrats! You are safe, stay alert and stay Inside as much as possible. Use masks while going outside, sanitize yourself and your belongings, avoid contamination from any thing. Drink hot water and keep yourself clean always</p>
+                        <p>Thank you</p>
+                    </div>
+                    <button className="corona-helpline">Corona HelpLine No: +234-234-7654</button>
+                </div>
+                
+            </div>
         </div>
     )
 }
